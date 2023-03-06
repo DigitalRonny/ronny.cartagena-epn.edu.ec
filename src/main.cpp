6bc78cdbed6a42e9ddbeda7a631d30b34aa130ba
DigitalRonny <rondamian04@gmail.com>
2023-03-06 13:09:16 -0500

    parte de excpeciones

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,62 @@ using namespace std;
 * @details  : Examen Bimestral II
 */
 
+//Constantes
 const string RCNOMBRE_COMPLETO = "Ronny Damian Cartagena Toaquiza";
+const int RCNUMERO_CEDULA = 1726340514;
 
+struct rcCoordenada{
+    int rcCapacidadBelica;
+    char rcGeo[4];
+    string rcDetalleArsenal;
+};
+
+struct Nodo{
+    rcCoordenada coordenada;//almacenar la estructura coordenada en el Nodo
+    Nodo *der;
+    Nodo *izq;
+};
+
+
+//procedimiento para leer archivo
+void rcLeerArchivo(){
+    string rcLinea;
+    ifstream rcFile;
+    bool rcFistLine;
+    rcFile.open("files\\coordenadas.txt", ios_base::in);
+    if (!rcFile.is_open())
+    {
+        cout<<"Error de abrir el archivo."<< endl;
+        exit(1);
+    }
+    else{
+        try
+        {
+            if (rcFistLine){
+                throw ">> Error:";//rcError
+                rcFistLine = false;
+            }
+            else{
+                do{
+                    getline( rcFile, rcLinea );// va a sacar una linea de f y la va a poner en s para leerla
+                    cout << rcLinea << endl;//se imprime la linea que esta en rcLinea
+
+                }while( !rcFile.eof() );//leer hasta end of file
+            rcFile.close();
+            }
+        }
+        catch(string rcError){
+        cout << "\x1b[31m" << ">> Error en la ope" << "\x1b[0m" << endl;
+    }
+    }
+    
+    
+
+}
 
 
 int main()
 {
-    
-    cout << "Hello world!" << endl;
+    rcLeerArchivo();
+    return 0;
 }
