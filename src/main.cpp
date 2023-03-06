260a3517a6563a4d100f0970480a42b629fd6248
DigitalRonny <rondamian04@gmail.com>
2023-03-06 12:20:30 -0500

    crear archivo

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include <windows.h> 
+#include<iostream>
+#include<stdlib.h>
+#include <fstream> //manejo archivos
+using namespace std;
+
+/*
+* @autor    : Ronny Cartagena
+* C.I       : 1726340514
+* @date     : 06/03/2023
+* @details  : Examen Bimestral II
+*/
+
+const string RCNOMBRE_COMPLETO = "Ronny Damian Cartagena Toaquiza";
+
+
+
+int main()
+{
+    
+    cout << "Hello world!" << endl;
+}
